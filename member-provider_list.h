4ff02e71af8a450e1fae4e171f85b4f90ea94ba6
Jeffrey Jernstrom <smidt@pdx.edu>
2021-02-19 15:43:24 -0800

    Started .h file for member and provider lists

---
+++ member-provider_list.h
@@ -0,0 +1,19 @@
+// Member and Provider Lists
+#include <vector>
+#include "person.h"
+
+class member_list
+{
+  public:
+
+  private:
+    vector member;
+};
+
+class member_list
+{
+  public:
+
+  private:
+    vector provider;
+};
