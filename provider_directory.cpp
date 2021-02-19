772bdaf19a0abbd545b7bc662c498f19ac53f31d
kotoriのねこ <2567702743@qq.com>
2021-02-19 21:58:18 +0000

    Key-value Pair implementation

--- provider_directory.cpp
+++ provider_directory.cpp
@@ -41,6 +41,138 @@ Pair<K,V>::~Pair()
 }
 
 
+template <class K, class V>
+void Pair<K,V>::setKey(const K& key)
+{
+  this->key = key;
+}
+
+
+template <class K, class V>
+const K& Pair<K,V>::getKey() const
+{
+  return key;
+}
+
+
+template <class K, class V>
+const V& Pair<K,V>::value() const
+{
+  return *data;
+}
+
+
+template <class K, class V>
+V& Pair<K,V>::value()
+{
+  return *data;
+}
+
+
+template <class K, class V>
+void Pair<K,V>::value(const V& data)
+{
+  if (this->data != NULL)
+    delete this->data;
+  this->data = new V(data);
+}
+
+
+template <class K, class V>
+int Pair<K,V>::compare(const K& key) const
+{
+  if (this->key > key)
+    return 1;
+  else if (this->key < key)
+    return -1;
+  return 0;
+}
+
+
+template <class K, class V>
+bool operator== (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) == 0;
+}
+
+
+template <class K, class V> 
+bool operator== (const Pair<K,V>& pair, const K& key)
+{
+  return key == pair;
+}
+
+
+template <class K, class V> 
+bool operator!= (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) != 0;
+}
+
+
+template <class K, class V> 
+bool operator!= (const Pair<K,V>& pair, const K& key)
+{
+  return key != pair;
+}
+
+
+template <class K, class V> 
+bool operator<= (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) >= 0;
+}
+
+
+template <class K, class V> 
+bool operator<= (const Pair<K,V>& pair, const K& key)
+{
+  return pair.compare(key) <= 0;
+}
+
+
+template <class K, class V> 
+bool operator>= (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) <= 0;
+}
+
+
+template <class K, class V> 
+bool operator>= (const Pair<K,V>& pair, const K& key)
+{
+  return pair.compare(key) >= 0;
+}
+
+
+template <class K, class V> 
+bool operator< (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) > 0;
+}
+
+
+template <class K, class V> 
+bool operator< (const Pair<K,V>& pair, const K& key)
+{
+  return pair.compare(key) < 0;
+}
+
+
+template <class K, class V> 
+bool operator> (const K& key, const Pair<K,V>& pair)
+{
+  return pair.compare(key) < 0;
+}
+
+
+template <class K, class V> 
+bool operator> (const Pair<K,V>& pair, const K& key)
+{
+  return pair.compare(key) > 0;
+}
+
+
 
 /** # TreeNoe */
 template <class K, class V>
