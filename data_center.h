72def25591057453a5863d8f711ef3163cb7b276
mackfoggia <mack.foggia@yahoo.com>
2021-02-20 12:21:02 -0800

    added h file for data center

---
+++ data_center.h
@@ -0,0 +1,35 @@
+#ifndef DATA_CENTER_H 
+#define DATA_CENTER_H
+
+#include <string>
+#include "member-provider_list.h"
+#include "provider_directory.h"
+
+
+class DataCenter{
+    public:
+        DataCenter();
+
+        //Start the program from either perspective
+        void providerMenu();
+        void managerMenu();
+
+    private:
+        //Objects
+        member_list memberList;
+        provider_list providerList;
+        ProviderDirectory providerDirectory;
+
+        //Internal helper functions
+        int updateLists();
+        int recordService();
+        int generateReport();
+
+        //UI helper functions
+        void UIPrompt(std::string);
+        void UI(std::string &, std::string);
+        void UI(int &, std::string);
+};
+
+
+#endif
