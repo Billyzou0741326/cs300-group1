30ab0e542876d9bbcb2aa0308dc1ea7eeed56881
Jeffrey Jernstrom <smidt@pdx.edu>
2021-02-22 17:49:21 -0800

    Polishing... :)

--- member-provider_list.h
+++ member-provider_list.h
@@ -9,6 +9,9 @@ public:
   member_list();
   ~member_list();
 
+  // Searches for a member with the given ID and if found populates the
+  // member object passed to the function
+  // Returns true for success, false for no match / empty list
   bool retrieve_member(int id, member &found);
 
   // Displays the Names and IDs of all the members in the list
@@ -16,25 +19,28 @@ public:
   bool display_all();
 
   // Adds a member to the list
-  // Returns true for success, false if a member of the same Name or ID exists
+  // Returns true for success, false if a member of the same ID exists -
+  // nothing is added
   bool add_member(member &toadd);
 
+  // Adds a service record to a member in the list with the given ID
+  // Returns 0 for success, 1 for no match / empty list, 2 if a service record
+  // with the same ID exist - nothing is added
   bool add_service(int member_id, service_record &toadd);
 
-  // Edits a member from the list
+  // Replaces a member from the list
   // Returns true for success, false for no match / empty list
   bool edit(int member_id, member &toupdate);
 
-  // Removes a member from the list given a name or ID
+  // Removes a member from the list given a member ID
   // Returns true for success, false for no match / empty list
   bool remove(int member_id);
 
-  // Determines if a member in the list has a valid membership
-  // given a Name or ID
+  // Determines if a member in the list has a valid membership given an ID
   // Returns 0 if valid, 1 for invalid, 2 for no match / empty list
   int validate(int member_id);
 
-  // Save the member list to file with then given filename
+  // Save the member list to file with the given filename
   // Returns true for success, false for failure
   bool save_list(string filename);
 
@@ -42,11 +48,14 @@ public:
   // Returns true for success, false for bad filename
   bool load_list(string filename);
 
-  // Member service report
+  // Member service report - List of a member's service record for the past 
+  // seven days. Creates a text file in Member_Reports directory.
+  // Returns true for success, false otherwise.
   bool generate_report(int member_id);
 
 private:
   vector member;
+
 };
 
 class provider_list
@@ -55,32 +64,34 @@ public:
   provider_list();
   ~provider_list();
 
-  bool retrieve_provider(int id, provider &found);
-
-  // Displays the Names and IDs of all the provider in the list
+  // Searches for a provider with the given ID and if found populates the
+  // provider object passed to the function
   // Returns true for success, false for no match / empty list
+  bool retrieve_provider(int provider_id, provider &found);
+
+  // Displays the Names and IDs of all the providers in the list
+  // Returns true for success, false for empty list
   bool display_all();
 
-  // Adds a member to the list
-  // Returns true for success, false if a provider of the same Name or ID exists
-  bool add_member(provider &toadd);
+  // Adds a provider to the list
+  // Returns true for success, false if a provider of the same ID exists - 
+  // nothing is added
+  bool add_provider(provider &toadd);
 
+  // Adds a service record to a provider in the list with the given ID
+  // Returns 0 for success, 1 for no match / empty list, 2 if a service record
+  // of the same ID exist - nothing is added
   bool add_service(int provider_id, service_record &toadd);
 
-  // Edits a provider from the list
+  // Replaces a provider from the list
   // Returns true for success, false for no match / empty list
-  bool edit(int provider_id, provider &toupdate);
+  bool edit_provider(int provider_id, provider &toupdate);
 
-  // Removes a member from the list given a name or ID
+  // Removes a provider from the list with the given ID
   // Returns true for success, false for no match / empty list
-  bool remove(int provider_id);
+  bool remove_provider(int provider_id);
 
-  // Determines if a member in the list has a valid membership
-  // given a Name or ID
-  // Returns 0 if valid, 1 for invalid, 2 for no match / empty list
-  int validate(int provider_id);
-
-  // Save the provider list to file with then given filename
+  // Save the provider list to file with the given filename
   // Returns true for success, false for failure
   bool save_list(string filename);
 
@@ -88,14 +99,20 @@ public:
   // Returns true for success, false for bad filename
   bool load_list(string filename);
 
-  // EFT Report - List of privider name, ID Number, Total Amout Charged for the
-  // past seven days. Text file in ETF_Reports directory.
+  // EFT Report - List of privider names, ID Numbers, Total Amout Charged for
+  // the past seven days 
+  // Creates a text file in ETF_Reports directory.
+  // Returns true for success, false otherwise.
+  bool generate_ETF_report();
   
-  // Accounting Report - List of providers that providing servies in the past
-  // seven days. Plus, the number of providers in this list, the total number
-  // of consultations, and the total dollar amout for all services provided.
-  // Text file in Accounting_Reports directory 
+  // Accounting Report - List of providers having provided services in the past
+  // seven days plus the number of providers in this list, the total number
+  // of consultations, and the total dollar amount for all services provided.
+  // Creates a text file in Accounting_Reports directory 
+  // Returns true for success, false otherwise
+  bool generate_accounting_report();
 
-  private:
-    vector provider;
+private:
+  vector provider;
+
 };
