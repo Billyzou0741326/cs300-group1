1c16923e7694687dcd5819390e7aa010a26a3479
kotoriのねこ <2567702743@qq.com>
2021-03-05 19:36:22 +0000

    ProviderDirectory detects duplicate service code on load; unit test
    * provider_directory: resolves duplicate service code on load
    * provider_directory: adds unit tests for duplicate service code

--- provider_directory_test.cpp
+++ provider_directory_test.cpp
@@ -1,9 +1,16 @@
+// ProviderDirectory Implementation
+// Original author: Billy Zou
+
 #include <sstream>
 #include <string>
 #include <gtest/gtest.h>
 #include "provider_directory.h"
 
 
+/** HandleCorrectInput
+ *
+ *  Test for loading correct input
+ */
 TEST(ProviderDirectoryLoadTest, HandleCorrentInput)
 {
   ProviderDirectory dir;
@@ -29,9 +36,15 @@ TEST(ProviderDirectoryLoadTest, HandleCorrentInput)
 
   status = dir.validateServiceCode(12347);
   EXPECT_EQ(status, true);
+
+  EXPECT_EQ(2, dir.size());
 }
 
 
+/** HandleIncompleteInput
+ *
+ *  Test for graceful failure on incomplete input
+ */
 TEST(ProviderDirectoryLoadTest, HandleIncompleteInput)
 {
   ProviderDirectory dir;
@@ -44,9 +57,15 @@ TEST(ProviderDirectoryLoadTest, HandleIncompleteInput)
 
   status = dir.load(input);
   EXPECT_EQ(status, false);
+
+  EXPECT_EQ(0, dir.size());
 }
 
 
+/** HandleInvalidFieldName
+ * 
+ *  Test for graceful failure on invalid field names
+ */
 TEST(ProviderDirectoryLoadTest, HandleInvalidFieldName)
 {
   ProviderDirectory dir;
@@ -61,9 +80,14 @@ TEST(ProviderDirectoryLoadTest, HandleInvalidFieldName)
 
   status = dir.load(input);
   EXPECT_EQ(status, false);
+  EXPECT_EQ(0, dir.size());
 }
 
 
+/** HandleInvalidType
+ *
+ *  Test for graceful failure on invalid type (expecting int, got string)
+ */
 TEST(ProviderDirectoryLoadTest, HandleInvalidType)
 {
   ProviderDirectory dir;
@@ -78,6 +102,37 @@ TEST(ProviderDirectoryLoadTest, HandleInvalidType)
 
   status = dir.load(input);
   EXPECT_EQ(status, false);
+  EXPECT_EQ(0, dir.size());
+}
+
+
+/** HandleDuplicateServiceCode
+ *
+ *  Test for conflict resolution on duplicate entries
+ */
+TEST(ProviderDirectoryLoadTest, HandleDuplicateServiceCode)
+{
+  ProviderDirectory dir;
+  bool status = false;
+  std::istringstream input(
+      "ServiceCode: 123123\n"
+      "ServiceName: name\n"
+      "Fees: 70.0\n"
+      "Description: description\n"
+      "----\n"
+      "ServiceCode: 123123\n"
+      "ServiceName: another name\n"
+      "Fees: 70.0\n"
+      "Description: description\n"
+      "----\n"
+  );
+
+  status = dir.load(input);
+  EXPECT_EQ(status, true);
+  EXPECT_EQ(1, dir.size());
+
+  status = dir.validateServiceCode(123123);
+  EXPECT_EQ(status, true);
 }
 
 
