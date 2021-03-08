e878301d004620dae5665d3bea37b10be6db4dcd
Jeffrey Jernstrom <smidt@pdx.edu>
2021-03-08 14:23:33 -0800

    Polishing

--- member-provider_list.cpp
+++ member-provider_list.cpp
@@ -26,35 +26,39 @@ member_list::member_list()
 
 bool member_list::retrieve_member(int member_id, member &found)
 {
+  // Find Member
   for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
-    if(mptr->compare(member_id)) {
-      mptr->copy(found);
+    if(mptr->compare(member_id)) {  // Found!
+      mptr->copy(found);  // Copy
       return true;  // Success
     }
   }
-  return false;  // Fail
+  return false;  // Fail - Empty list / no match
 }
 
 
 
 bool member_list::display_all()
 {
+  // Check empty
   if(mList.empty())
     return false;  // Fail - Empty list
 
+  // Display all
   for(mptr = mList.begin(); mptr != mList.end(); ++mptr)
     mptr->display_basic(); 
 
-  return true;
+  return true;  // Success
 }
 
 
 
 bool member_list::add_member(member &toadd)
 {
+  // Add member
   mList.push_front(toadd);
 
-  return true;
+  return true;  // Success
   // Not sure this can fail...
 }
 
@@ -62,17 +66,25 @@ bool member_list::add_member(member &toadd)
 
 bool member_list::add_service(int member_id, ServiceRecord &toadd)
 {
+  // Find member
+  for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
+    if(mptr->compare(member_id)) {  // Found!
+      mptr->add_service(toadd);  // Add service
+      return true;  // Success
+    }
+  }
 
-  return false;
+  return false;  // Fail - Empty List / No Match
 }
 
 
 
 bool member_list::edit_member(int member_id, member &toupdate)
 {
+  // Find member
   for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
-    if(mptr->compare(member_id)) {
-      mptr->edit(toupdate);
+    if(mptr->compare(member_id)) {  // Found!
+      mptr->edit(toupdate);  // Update
       return true;  // Success
     }
   }
@@ -83,8 +95,9 @@ bool member_list::edit_member(int member_id, member &toupdate)
 
 bool member_list::remove_member(int member_id)
 {
+  // Find member
   for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
-    if(mptr->compare(member_id)) {
+    if(mptr->compare(member_id)) {  // Found!
       mList.erase(mptr); // Delete
       return true;  // Success
     } 
@@ -101,8 +114,9 @@ int member_list::validate_member(int member_id)
   if (member_id < 0 || member_id > 999999999)
     return 3;  // Fail - member_id out of range
     
+  // Find member
   for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
-    if(mptr->compare(member_id)) {
+    if(mptr->compare(member_id)) {  // Found!
       if(mptr->validate()) 
         return 0;  // Validated
       else 
@@ -117,12 +131,14 @@ int member_list::validate_member(int member_id)
 
 int member_list::save_list(string filename)
 {
+  // Open File
   ofstream write;
   write.open(filename);
 
   // Ensure Connection
   if(write) { 
       
+    // Write
     for(mptr = mList.begin(); mptr != mList.end(); ++mptr) {
       if(!mptr->save_info(write))
         return false; // Fail - Write error
@@ -140,26 +156,27 @@ int member_list::save_list(string filename)
 
 int member_list::load_list(string filename)
 {
+  // Open File
   ifstream read;
   read.open(filename);
   
   // Ensure Connection
   if(read) {
-    member temp;
+    member temp;  // Hold data to add to the list
 
+    // Read
     while(temp.load_member(read)) {
-      add_member(temp);
+      if(!add_member(temp))  // Add to list
+        return 1;  // Fail - File read error
     }
     
-    // 3 fail modes?
-
     read.close();  // Close the file
     read.clear();  // Recycle var
     
-    return true;  //Success
+    return 0;  //Success
   }
   
-  return false;  // Fail - File open error
+  return 2;  // Fail - File open error
 }
 
 
@@ -172,9 +189,10 @@ int member_list::generate_member_report(int member_id)
 
       //TODO: generate filename
       // Generate filename and open file
+      string directory = "Member_Reports/";
       string filename = "addFileName";
       ofstream write;
-      write.open(filename);
+      write.open(directory + filename);
 
       // Ensure Connection and write report 
       if(write) { 
@@ -192,7 +210,7 @@ int member_list::generate_member_report(int member_id)
     return 1;  // Fail - File open error
   }
 
-  return 2;  // Fail - No match
+  return 2;  // Fail - No Match / Empty List
 }
 
  
@@ -211,35 +229,39 @@ provider_list::provider_list()
 
 bool provider_list::retrieve_provider(int provider_id, provider &found)
 {
+  // Find Provider
   for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
-    if(pptr->compare(provider_id)) {
+    if(pptr->compare(provider_id)) {  // Found!
       pptr->copy(found);
       return true;  // Success
     }
   }
-  return false;  // Fail
+  return false;  // Fail - Empty List / No match
 }
 
 
 
 bool provider_list::display_all()
 {
+  // Check empty
   if(pList.empty())
     return false;  // Fail - Empty list
 
+  // Display all
   for(pptr = pList.begin(); pptr != pList.end(); ++pptr)
     pptr->display_basic();
 
-  return true;
+  return true;  // Success
 }
 
 
 
 bool provider_list::add_provider(provider &toadd)
 {
+  // Add Provider
   pList.push_front(toadd);
 
-  return true;
+  return true;  // Success
   // Not sure this can fail...
 }
 
@@ -247,8 +269,15 @@ bool provider_list::add_provider(provider &toadd)
 
 bool provider_list::add_service(int provider_id, ServiceRecord &toadd)
 {
+  // Find provider
+  for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
+    if(pptr->compare(provider_id)) {  // Found!
+      pptr->add_service(toadd);  // Add service
+      return true;  // Success
+    }
+  }
 
-  return false;
+  return false;  // Fail - Empty List / No Match
 }
 
 
@@ -258,8 +287,7 @@ bool provider_list::edit_provider(int provider_id, provider &toupdate)
   // Find provider
   for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
     if(pptr->compare(provider_id)) {  // Found!
-      // Edit provider
-      pptr->edit(toupdate);
+      pptr->edit(toupdate);  // Update
       return true;  // Success
     }
   }
@@ -273,8 +301,7 @@ bool provider_list::remove_provider(int provider_id)
   // Find provider  
   for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
     if(pptr->compare(provider_id)) {  // Found!
-      // Remove provider
-      pList.erase(pptr);
+      pList.erase(pptr);  // Delete
       return true;  // Success
     } 
   }
@@ -290,10 +317,10 @@ int provider_list::validate_provider(int provider_id)
   if (provider_id < 0 || provider_id > 999999999)
     return 3;  // Fail - provider_id out of range
 
+  // Find provider
   for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
-    if(pptr->compare(provider_id)) {
+    if(pptr->compare(provider_id)) {  // Found!
       if(pptr->validate())
-      if(true)
         return 0;  // Validated
       else 
         return 1;  // Not Validated
@@ -307,12 +334,14 @@ int provider_list::validate_provider(int provider_id)
 
 bool provider_list::save_list(string filename)
 {
+  // Open File
   ofstream write;
   write.open(filename);
 
   // Ensure Connection
   if(write) { 
       
+    // Write 
     for(pptr = pList.begin(); pptr != pList.end(); ++pptr) {
       if(!pptr->save_info(write))
         return false; // Fail - Write error
@@ -330,19 +359,20 @@ bool provider_list::save_list(string filename)
 
 int provider_list::load_list(string filename)
 {
+  // Open File
   ifstream read;
   read.open(filename);
   
   // Ensure Connection
   if(read) {
-    provider temp;
+    provider temp;  // Hold data to add to the list
 
+    // Read
     while(temp.load_provider(read)) {
-      add_provider(temp);
+      if(!add_provider(temp));  // Add to the list
+      return 1;  // Fail - File read error
     }
     
-    // 3 fail modes?
-
     read.close();  // Close the file
     read.clear();  // Recycle var
     
@@ -383,7 +413,7 @@ int provider_list::generate_provider_report(int provider_id)
     return 1;  // Fail - File open error
   }
 
-  return 2;  // Fail - No match / empty list
+  return 2;  // Fail - No Match / Empty List
 }
 
 
