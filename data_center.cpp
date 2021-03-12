1389ab136e345fe6fbcfe016d80387e324883657
mackfoggia <mack.foggia@yahoo.com>
2021-03-11 18:25:45 -0800

    Expanded error code utilization in report generation

--- data_center.cpp
+++ data_center.cpp
@@ -181,31 +181,36 @@ void DataCenter::managerMenu(){
             //Member Report
             case 5:
                 UI(var, "Enter member number", 9, 9);
-                if(!memberList.generate_member_report(var))
-                    UI("Report generation failure! Check input.");
+                retval = memberList.generate_member_report(var);
+                if(retval == 1) UI("There was a problem opening the file.");
+                else if(retval == 2) UI("That user was not found.");
                 else UI("Report successfully generated.");
                 break;
 
             //Provider Report
             case 6:
                 UI(var, "Enter provider number", 9, 9);
-                if(!providerList.generate_provider_report(var))
-                    UI("Report generation failure! Check input.");
+                retval = providerList.generate_provider_report(var);
+                if(retval == 1) UI("There was a write problem.");
+                else if(retval == 2) UI("There was a problem opening the file.");
+                else if(retval == 3) UI("That user was not found.");
                 else UI("Report successfully generated.");
                 break;
 
             //EFT Report
             case 7:
-                if(!providerList.generate_ETF_report())
-                    UI("Record generation failure! Check input.");
-                else UI("EFT record successfully generated.");
+                retval = providerList.generate_ETF_report();
+                if(retval == 1) UI("There was a problem opening the file.");
+                else if(retval == 2) UI("That user was not found.");
+                else UI("Report successfully generated.");
                 break;
 
             //Accounting Report
             case 8:
-                if(!providerList.generate_accounting_report())
-                    UI("Report generation failure! Check input.");
-                else UI("Accounting report successfully generated.");
+                retval = providerList.generate_accounting_report();
+                if(retval == 1) UI("There was a problem opening the file.");
+                else if(retval == 2) UI("That user was not found.");
+                else UI("Report successfully generated.");
                 break;
 
             //Exit
